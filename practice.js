0beeb08ae526a706ffd4d06ee61ade5b7251c377
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-25 18:27:18 +0100

    Chaining Method

--- practice.js
+++ practice.js
@@ -4,7 +4,10 @@ const movies = [
   { title: 'c', year: 2018, rating: 3 },
   { title: 'd', year: 2017, rating: 4.5 },
 ];
-
+// All the movies in 2018 with rating > 4
+// Sort them by their rating
+// Descending order
+// Pick their title
 const title = movies
   .filter((m) => m.year === 2018 && m.rating >= 4)
   .sort((a, b) => a.rating - b.rating)
