391cf1832531c13a287a3d2cff577431eb8d7fd1
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-27 19:16:25 +0100

    Exercise Move Element

--- style.js
+++ style.js
@@ -1,16 +1,19 @@
 console.clear();
 
-const numbers = [1, 2, 3, 4, 1];
+const numbers = [1, 2, 3, 4];
+const output = move(numbers, 0, 0);
 
-const count = countOccurrences(numbers, 1);
+console.log(output);
 
-console.log(count);
+function move(array, index, offset) {
+  const position = index + offset;
+  if (position >= array.length || position < 0) {
+    console.error('Invalid offset');
+    return;
+  }
 
-function countOccurrences(array, searchElement) {
-  return array.reduce((accumulator, current) => {
-    const occurrence =
-      current === searchElement ? 1 : 0;
-    console.log(accumulator, current, searchElement);
-    return accumulator + occurrence;
-  }, 0);
+  const output = [...array];
+  const element = output.splice(index, 1)[0];
+  output.splice(position, 0, element);
+  return output;
 }
