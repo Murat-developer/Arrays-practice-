93fe9d86c2d6f470bb4cc170af67e80c88194ec8
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-27 18:13:37 +0100

    get max ( reduce() method)

--- practice.js
+++ practice.js
@@ -1,15 +1,192 @@
-const circle = {
-  radius: 1,
-  draw() {
-    console.log('draw');
+console.clear();
+
+const person = [
+  {
+    name: 'John Smith',
+    gender: 'male',
+    age: 34,
+    height: 158,
+    eyes_color: 'green',
+    weigth: 76,
   },
-};
+  {
+    name: 'Mary Popins',
+    gender: 'female',
+    age: 25,
+    height: 167,
+    eyes_color: 'blue',
+    weigth: 55,
+  },
+  {
+    name: 'William Phill',
+    gender: 'male',
+    age: 40,
+    height: 178,
+    eyes_color: 'brown',
+    weigth: 85,
+  },
+];
+//***MAP***
+//1. Get array of all names
+const allNames = person.map((value) => value.name);
+// console.log(allNames);
+//2. Get array of all heights
+const allHeights = person.map((value) => value.height);
+// console.log(allHeights);
+//3. Get array of objects with just name and height properties
+const nameAndHeigth = person.map((object) => ({
+  name: object.name,
+  height: object.height,
+}));
+// console.log(nameAndHeigth);
+//4. Get array of all first names
+const firstName = person.map(
+  (value) => value.name.split(' ')[0],
+);
+// console.log(firstName);
 
-// for (let key in circle) console.log(key, circle[key]);
+//***REDUCE***
+//1. Get total mass of all characters
+const totalAge = person.reduce(
+  (accum, curr) => accum + curr.age,
+  0,
+);
+// console.log(totalAge);
+//2. Get total height of all characters
+const totalHeight = person.reduce(
+  (accum, curr) => accum + curr.height,
+  0,
+);
+// console.log(totalHeight);
+//3. Get total number of characters by eye color
+const totalEyesColor = person.reduce((accum, curr) => {
+  const color = curr.eyes_color;
+  if (accum[color]) accum[color]++;
+  else accum[color] = 1;
 
-// for (let key of Object.keys(circle)) console.log(key);
+  return accum;
+}, {});
+// console.log(totalEyesColor);
+//4. Get total number of characters in all the character names
+const sumName = person.reduce((accum, curr) => {
+  const countName = curr.name;
+  if (accum[countName]) accum[countName]++;
+  else accum[countName] = 1;
 
-// for (let entries of Object.entries(circle))
-//   console.log(entries);
+  return accum;
+}, {});
+// console.log(sumName);
 
-if ('radius' in circle) console.log('yes');
+const totalName = person.reduce((accum, curr) => {
+  const countGender = curr.gender;
+  if (accum[countGender]) accum[countGender]++;
+  else accum[countGender] = 1;
+
+  return accum;
+}, {});
+// console.log(totalName);
+
+//***FILTER***
+//1. Get characters with mass greater than 100
+const foundWeigth = person.filter(
+  (person) => person.weigth < 85,
+);
+// console.log(foundWeigth);
+//2. Get characters with height less than 200
+const foundHeigth = person.filter(
+  (person) => person.height > 170,
+);
+// console.log(foundHeigth);
+//3. Get all male characters
+const onlyMale = person.filter(
+  (person) => person.gender !== 'female',
+);
+// console.log(onlyMale);
+
+//4. Get all female characters
+const onlyFemale = person.filter(
+  (person) => person.gender === 'female',
+);
+// console.log(onlyFemale);
+
+//***SORT***
+//1. Sort by mass
+const sortByWeigth = person.sort(
+  (a, b) => b.weigth - a.weigth,
+);
+// console.log(sortByWeigth);
+
+//2. Sort by height
+const sortByHeight = person.sort(
+  (a, b) => a.height - b.height,
+);
+// console.log(sortByHeight);
+//3. Sort by name
+const sortByName = person.sort(
+  (a, b) => a.name - b.name,
+);
+// console.log(sortByName);
+
+//4. Sort by gender
+const sortedByGender = person.sort(
+  (a, b) => a.gender - b.gender,
+);
+// console.log(sortedByGender);
+
+//***EVERY***
+//1. Does every character have blue eyes?
+const checkBlueEyes = person.every(
+  (person) => person.eyes_color === 'blue',
+);
+// console.log(checkBlueEyes);
+//2. Does every character have mass more than 40?
+const checkWeight = person.every(
+  (person) => person.weigth > 50,
+);
+// console.log(checkBlueEyes);
+//3. Is every character shorter than 200?
+const checkHeight = person.every(
+  (person) => person.height < 200,
+);
+// console.log(checkHeight);
+//4. Is every character male?
+const foundGender = person.every(
+  (person) => person.gender === 'male',
+);
+// console.log(foundGender);
+
+//***SOME***
+//1. Is there at least one male character?
+const atLeastOneMale = person.some(
+  (person) => person.gender === 'male',
+);
+// console.log(atLeastOneMale);
+
+//2. Is there at least one character with blue eyes?
+const foundEyesColor = person.some(
+  (person) => person.eyes_color === 'blue',
+);
+// console.log(foundEyesColor);
+
+//3. Is there at least one character taller than 210?
+const foundHeight = person.some(
+  (person) => person.height > 200,
+);
+// console.log(foundHeight);
+
+//4. Is there at least one character that has mass less than 50?
+const foundWeight = person.some(
+  (person) => person.weigth < 50,
+);
+// console.log(foundWeight);
+
+const aboutPerson = person
+  .filter(
+    (person) =>
+      person.gender === 'male' && person.age > 30,
+  )
+  .sort((a, b) => a.height - b.height)
+  .reverse()
+  .map((person) => person.name.split(' ')[0]);
+
+console.log(aboutPerson);
--- style.js
+++ style.js
@@ -1,78 +1,13 @@
-const characters = [
-  {
-    name: 'Luke Skywalker',
-    height: '172',
-    mass: '77',
-    eye_color: 'blue',
-    gender: 'male',
-  },
-  {
-    name: 'Darth Vader',
-    height: '202',
-    mass: '136',
-    eye_color: 'yellow',
-    gender: 'male',
-  },
-  {
-    name: 'Leia Organa',
-    height: '150',
-    mass: '49',
-    eye_color: 'brown',
-    gender: 'female',
-  },
-  {
-    name: 'Anakin Skywalker',
-    height: '188',
-    mass: '84',
-    eye_color: 'blue',
-    gender: 'male',
-  },
-];
+console.clear();
 
-//***MAP***
-//1. Get array of all names
-//2. Get array of all heights
-//3. Get array of objects with just name and height properties
-//4. Get array of all first names
+const numbers = [1, 2, 3, 4];
 
-//***REDUCE***
-//1. Get total mass of all characters
-//2. Get total height of all characters
-//3. Get total number of characters by eye color
-//4. Get total number of characters in all the character names
+const max = getMax([1, 2, 3, 4]);
 
-//***FILTER***
-//1. Get characters with mass greater than 100
-//2. Get characters with height less than 200
-//3. Get all male characters
-//4. Get all female characters
+console.log(max);
 
-//***SORT***
-//1. Sort by mass
-//2. Sort by height
-//3. Sort by name
-//4. Sort by gender
+function getMax(array) {
+  if (array.length === 0) return undefined;
 
-//***EVERY***
-//1. Does every character have blue eyes?
-//2. Does every character have mass more than 40?
-//3. Is every character shorter than 200?
-//4. Is every character male?
-
-//***SOME***
-//1. Is there at least one male character?
-//2. Is there at least one character with blue eyes?
-//3. Is there at least one character taller than 210?
-//4. Is there at least one character that has mass less than 50?
-
-const array = [15, 16, 17, 18, 19];
-
-function reducer(previous, current, index) {
-  const returns = previous + current;
-  console.log(
-    `previous: ${previous}, current: ${current}, index: ${index}, returns: ${returns}`,
-  );
-  return returns;
+  return array.reduce((a, b) => (a > b ? a : b), 0);
 }
-
-array.reduce(reducer);
