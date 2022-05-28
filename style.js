b2130fe4361f48865a429ec9d6c55b7f2a06055a
Murat-developer <78870265+Murat-developer@users.noreply.github.com>
2022-05-28 15:37:17 +0100

    Arguments

--- style.js
+++ style.js
@@ -1,16 +1,43 @@
 console.clear();
 
-const numbers = [1, 2, 3, 4];
+// const person = [
+//   {
+//     name: 'Mary Smith',
+//     age: 30,
+//     mass: 65,
+//     height: 170,
+//     eyes_color: 'brown',
+//     gender: 'female',
+//   },
+//   {
+//     name: 'William Clarck',
+//     age: 33,
+//     mass: 78,
+//     height: 178,
+//     eyes_color: 'blue',
+//     gender: 'male',
+//   },
+//   {
+//     name: 'Kate Querel',
+//     age: 34,
+//     mass: 68,
+//     height: 173,
+//     eyes_color: 'brown',
+//     gender: 'female',
+//   },
+//   {
+//     name: 'John Smith',
+//     age: 38,
+//     mass: 84,
+//     height: 180,
+//     eyes_color: 'green',
+//     gender: 'male',
+//   },
+// ];
 
-const output = except(numbers, [1]);
-
-console.log(output);
-
-function except(array, excluded) {
-  const output = [];
-  for (let element of array)
-    if (!excluded.includes(element))
-      output.push(element);
-
-  return output;
+function sum() {
+  let total = 0;
+  for (let value of arguments) total += value;
+  return total;
 }
+console.log(sum(1, 2, 3, 4, 5, 6));
